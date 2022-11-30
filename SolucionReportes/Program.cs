780f7840c2238dc6d130488a7b8459c5a1cdbd6b
Javo924 <105120945+Javo924@users.noreply.github.com>
2022-11-30 08:14:16 -0600

    Add files via upload

---
+++ SolucionReportes/Program.cs
@@ -0,0 +1,35 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using ProyectoPDF.Extension;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var context = new CustomAssemblyLoadContext();
+context.LoadUnmanagedLibrary(Path.Combine(Directory.GetCurrentDirectory(), "LibreriaPDF/libwkhtmltox.dll"));
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+//IWebHostEnvironment env = app.Environment;
+//Rotativa.AspNetCore.RotativaConfiguration.Setup(env.WebRootPath, "Rotativa");
+
